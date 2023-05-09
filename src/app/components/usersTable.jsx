7ecf2b0cb8ad0f247c97f7db079f4f604467bec3
ZagSerSan <yan.steys1997@gmail.com>
2023-05-09 10:06:33 +0200

    replacing code from App.js in users.jsx

--- src/app/components/usersTable.jsx
+++ src/app/components/usersTable.jsx
@@ -1,41 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import User from './user'
-import TableHeader from './tableHeader'
-import TableBody from './tableBody'
+import Table from './table'
 
 const UsersTable = ({
   users,
-  onDelete,
-  onToggleBookMark,
+  thState,
   sortSettings,
   onSetSortSettings,
-  thState,
   onSetThState
 }) => {
 
   return (<>
-    <table className="table">
-      <TableHeader {...{sortSettings,onSetSortSettings,thState,onSetThState}}/>
-      <TableBody {...{data: users, thState}} />
-      {/* <tbody>
-        {users.map(user => (
-          <User
-            key={user._id}
-            user={user}
-            onHandleDelete={onDelete}
-            onToggleBookMark={onToggleBookMark}
-          />
-        ))}
-      </tbody> */}
-    </table>
+    <Table {...{sortSettings,onSetSortSettings,thState,onSetThState,users}}/>
   </>)
 }
 
 UsersTable.propTypes = {
   users: PropTypes.oneOfType([PropTypes.array,PropTypes.object]),
-  onDelete: PropTypes.func.isRequired,
-  onToggleBookMark: PropTypes.func.isRequired,
   sortSettings: PropTypes.object.isRequired,
   onSetSortSettings: PropTypes.func.isRequired,
   thState: PropTypes.object.isRequired,
