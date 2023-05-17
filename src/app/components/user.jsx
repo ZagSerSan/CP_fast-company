18481bc3013453e8beadb2507449e927b130bfb0
ZagSerSan <yan.steys1997@gmail.com>
2023-05-17 08:21:11 +0200

    main logic

--- src/app/components/user.jsx
+++ src/app/components/user.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import "bootstrap/dist/css/bootstrap.css"
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 const User = ({userCrop,userId}) => {
-  const userItem = userCrop.find(item => item._id === userId)
+  // useEffect(()=> {
+    const userItem = userCrop.find(item => item._id === userId)
+  // },[userCrop])
   // console.log('userItem', userItem);
   // console.log('userId', userId);
   return (
-    <h3>{userItem.name}</h3>
+    <>
+      <h2>{userItem.name}</h2>
+      <h4>Профессия: {userItem.profession.name}</h4>
+      {userItem.qualities.map(item => (
+        <h4 className={'badge bg-'+item.color+' m-1'}>{item.name}</h4>
+      ))}
+      <h5>Completed Meetings: {userItem.completedMeetings}</h5>
+      <h5>Rate: {userItem.rate}</h5>
+      <Link className='btn btn-secondary' to='/Users'>All users</Link>
+    </>
   )
 }
  
