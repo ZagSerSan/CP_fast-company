952f6df4c4f2eb9abfc18583a2941d99a0e40f03
ZagSerSan <yan.steys1997@gmail.com>
2023-05-23 11:19:59 +0200

    valuesState for form

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 import './login.css'
 
 const Login = () => {
+  const [values, setValues] = useState({
+    mail: '',
+    password: ''
+  })
+  const {mail, password} = values;
+  
+  const changeValue = ({target}) => {
+    setValues(prevState => (
+      {
+        ...prevState,
+        [target.name]: target.value,
+      }
+    ))
+  }
+
   return ( 
     <form className='form' action="">
       <div className='form-row'>
         <label htmlFor='login'>Login:</label>
-        <input type="text" id='login'/>
+        <input type="text" id='login' name='mail' value={mail} onChange={changeValue}/>
       </div>
       <div className='form-row'>
         <label htmlFor='password'>Password:</label>
-        <input type="password" id='password'/>
+        <input type="password" id='password' name='password' value={password} onChange={changeValue}/>
       </div>
     </form>
    )
