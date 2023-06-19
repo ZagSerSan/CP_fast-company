295031b496aee4f6e93418c5b9659634366bc757
ZagSerSan <yan.steys1997@gmail.com>
2023-06-19 17:06:56 +0200

    qualities

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -25,41 +25,59 @@ const UserPage = ({ userId }) => {
       <div className="row gutters-sm mt-2">
 
         <div className="col-md-4 mb-3">
-          {selectedUser ? (
+          {selectedUser ? (<>
             <div className="card mb-3">
-            <div className="card-body">
-              <Link
-                to={`${userId}/edit`}
-                className="position-absolute top-0 end-0 btn btn-light btn-sm"
-                style={{zIndex: '1'}}
-              >
-                <i className="bi bi-gear"></i>
-              </Link>
-              <div className="d-flex flex-column align-items-center text-center position-relative">
-                <img
-                  src={`https://avatars.dicebear.com/api/avataaars/${(
-                    Math.random() + 1
-                  )
-                    .toString(36)
-                    .substring(7)}.svg`}
-                  className="rounded-circle shadow-1-strong me-3"
-                  alt="avatar"
-                  width="120"
-                  height="120"
-                />
-                <div className="mt-3">
-                  <h4>{selectedUser.name}</h4>
-                  <p className="text-secondary mb-1">{selectedUser.profession.name}</p>
-                  <div className="text-muted">
-                    <i className="bi bi-caret-down-fill text-primary" role="button"></i>
-                    <i className="bi bi-caret-up text-secondary" role="button"></i>
-                    <span className="ms-2">{selectedUser.rate}</span>
+              <div className="card-body">
+                <Link
+                  to={`${userId}/edit`}
+                  className="position-absolute top-0 end-0 btn btn-light btn-sm"
+                  style={{zIndex: '1'}}
+                >
+                  <i className="bi bi-gear"></i>
+                </Link>
+                <div className="d-flex flex-column align-items-center text-center position-relative">
+                  <img
+                    src={`https://avatars.dicebear.com/api/avataaars/${(
+                      Math.random() + 1
+                    )
+                      .toString(36)
+                      .substring(7)}.svg`}
+                    className="rounded-circle shadow-1-strong me-3"
+                    alt="avatar"
+                    width="120"
+                    height="120"
+                  />
+                  <div className="mt-3">
+                    <h4>{selectedUser.name}</h4>
+                    <p className="text-secondary mb-1">{selectedUser.profession.name}</p>
+                    <div className="text-muted">
+                      <i className="bi bi-caret-down-fill text-primary" role="button"></i>
+                      <i className="bi bi-caret-up text-secondary" role="button"></i>
+                      <span className="ms-2">{selectedUser.rate}</span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
+            <div className="card mb-3">
+              <div className="card-body d-flex flex-column justify-content-center text-center">
+                <h5 className="card-title">
+                    <span>Qualities</span>
+                </h5>
+                <p className="card-text">
+                {selectedUser.qualities.map((item) => (
+                  <h4
+                    key={item._id}
+                    className={'badge bg-' + item.color}
+                    style={{ fontSize: '16px', margin: '5px 5px 10px 0' }}
+                  >
+                    {item.name}
+                  </h4>
+                ))}
+                </p>
+              </div>
             </div>
-
+            </>
             // <div className="user-page">
             //   <h2 className="user-name">
             //     <b>{selectedUser.name}</b>
