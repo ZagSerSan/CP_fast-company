4fa8219095e819c7c10b50e264a04a7f38a882c4
ZagSerSan <yan.steys1997@gmail.com>
2023-08-07 09:43:17 +0200

    3. Создаем Qualities Slice

---
+++ src/app/store/qualities.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const qualitiesSlice = createSlice({
+  name: 'qualities',
+  initialState: {
+    entitites: null,
+    isLoading: true
+  }
+})
+
+const {reducer: qualitiesReducer} = qualitiesSlice
+
+export default qualitiesReducer
