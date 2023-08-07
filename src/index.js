33058cdbe3949a325e2930be345b3a2227fb7d8b
ZagSerSan <yan.steys1997@gmail.com>
2023-08-07 09:35:05 +0200

    2. Создаем Store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min'
-import App from './app/App'
 import reportWebVitals from './reportWebVitals'
 import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './app/App'
+import { createStore } from './app/store/createStore'
+import { Provider } from 'react-redux'
+
+const store = createStore()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
