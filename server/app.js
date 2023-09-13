1c8523e2aa82be18dac47138c5c5035b17d04332
ZagSerSan <yan.steys1997@gmail.com>
2023-09-13 17:13:23 +0200

    10. Создаем структуру роутов

--- server/app.js
+++ server/app.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose')
 const config = require('config')
 const chalk = require('chalk')
 const initDatabase = require('./startUp/initDatabase')
+const routes = require('./routes')
 
 const app = express()
 const PORT = config.get('port') ?? 8080
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use('/api', routes)
 
 // if (process.env.NODE_ENV === 'production') {
 //   console.log('production')
