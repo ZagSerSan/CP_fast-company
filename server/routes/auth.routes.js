a4e72bbf326a2f2fbbd8a00eebd03ac3e524d306
ZagSerSan <yan.steys1997@gmail.com>
2023-09-13 21:25:32 +0200

    1. Пишем логику для регистрации

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,47 @@
 const express = require('express')
 const router = express.Router({mergeParams: true})
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const { generateUserData } = require('../utils/helpers')
+const { TokenService } = require('../services/token.service')
+
+//todo что нужно сделать в signUp
+// 1. получить data из запроза, req: (email, password, ...)
+// 2. проверить, существует ли такой пользователь, чтобы не создать два одинаковых
+// 3. создать захешированный пароль, чтобы не хранить прямой пароль
+// 4. создать пользователя
+// 5. создать токены в локал стор для авторизации
 
 router.post('/signUp', async (req, res) => {
+  try {
+    const { email, password } = req.body // 1
+    const existUser = await User.findOne({ email }) // 2
 
+    if (existUser) {
+      // если пользователь найден, то отвечаем обратно клиенту и меняем статус на ошибку
+      return res.status(400).json({
+        error: {
+          message: 'EMAIL_EXISTS',
+          code: 400
+        }
+      })
+    }
+
+    // если предыдущий if не выполнился, знач всё нормально и продолжаем процесс регистрации
+    // 3. шифровка пароля (пароль, число-сложность шифрования)
+    const hashedPassword = bcrypt.hash(password, 12)
+    // 4
+    const newUser = User.create({
+      ...generateUserData(),
+      ...req.body,
+      password: hashedPassword
+    })
+    // 5
+    const tokens = TokenService.generate({ _id: newUser._id })
+    res.status(201).send({...tokens, userId: newUser._id})
+  } catch (e) {
+    
+  }
 })
 router.post('/signInWithPassword', async (req, res) => {
 
